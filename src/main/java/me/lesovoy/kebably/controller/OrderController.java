b46226623e28b98fa76dddd774a3ca4788deebb4
Konstantins Lesovojs <konstantins.lesovojs@accenture.com>
2019-04-17 15:05:53 +0300

    form prototype

--- src/main/java/me/lesovoy/kebably/controller/OrderController.java
+++ src/main/java/me/lesovoy/kebably/controller/OrderController.java
@@ -1,10 +1,11 @@
 package me.lesovoy.kebably.controller;
 
+import me.lesovoy.kebably.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,8 +17,13 @@ public class OrderController {
         return "menu";
     }
 
-    @RequestMapping(value = "/order", method = RequestMethod.POST)
-    public String order() {
-        return null;
+    @PostMapping("/")
+    public String order(Model model, @ModelAttribute Order order) {
+        model.addAttribute("name", null);
+        System.out.println(order.toString());
+        //TODO: implement form field mapping into internal model classes
+        //TODO: generate qrcode to /resources/img/qr.png
+
+        return "menu";
     }
 }
